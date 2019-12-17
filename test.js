ffb7c33a8b9fe7349f450bdb094a4655de2d6d67
Benjamin Dehlin <ben.dehlin@gmail.com>
2019-12-16 17:43:50 -0700

    added test.js with printing to console

--- test.js
+++ test.js
@@ -1,1 +1,6 @@
-console.log("this is a test")
+console.log("this is a practice shortcuts 1")
+console.log("this is a practice shortcuts 2")
+console.log("this is a practice shortcuts 3")
+console.log("this is a practice shortcuts 4")
+console.log("this is a practice shortcuts 5")
+console.log("this is a practice shortcuts 6")
